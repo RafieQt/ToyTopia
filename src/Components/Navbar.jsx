935efdf7487a63935e4fe8507244c5061d8de820
RafieQt <tajuddinrafi76@gmail.com>
2026-02-01 23:46:35 +0600

    navbar user image showing

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,18 +6,20 @@ import { toast } from 'react-toastify';
 
 const Navbar = () => {
     const { user, logoutUser } = use(AuthContext);
-    
-    const handleLogout = ()=>{
+
+    const handleLogout = () => {
         toast("logout");
         logoutUser();
     }
-    
+
+
+
     return (
         <div >
             <div className="navbar bg-base-100 shadow-sm px-10 flex items-center justify-between">
                 <div className="">
                     <Link className="btn text-blue-600 btn-ghost text-xl">ToyTopia</Link>
-                    
+
                 </div>
                 <div className='flex gap-3'>
                     <NavLink to={'/'} className={({ isActive }) => isActive ? 'text-blue-600' : 'text-black'}>Home</NavLink>
@@ -27,12 +29,21 @@ const Navbar = () => {
                 <div className="">
                     {
                         user ? <div className="flex items-center gap-2">
-                            <img className='w-10 rounded-full'
-                                alt=""
-                                src={user.photoURL} />
-                        <button onClick={handleLogout} className='btn bg-blue-500 px-4 w-25 text-white hover:bg-blue-600 scale-105 transition-transform shadow-lg'>Logout</button>
+                            <img
+                                className="w-10 rounded-full cursor-pointer"
+                                alt={user.displayName || "User"}
+                                src={user.photoURL}
+                                title={user.displayName} // <-- This shows the name on hover
+                            />
+
+                            <button
+                                onClick={handleLogout}
+                                className="btn bg-blue-500 px-4 w-25 text-white hover:bg-blue-600 scale-105 transition-transform shadow-lg"
+                            >
+                                Logout
+                            </button>
                         </div>
-                        :
+                            :
 
                             <Link to='/signin'>
                                 <button className='btn w-25  px-4 bg-blue-500 text-white hover:bg-blue-600 scale-105 transition-transform shadow-lg'>
